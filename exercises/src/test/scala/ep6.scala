a75acafb22db4d59a68a0e6187fcc94d330145c8
Jakub Kozłowski <kubukoz@gmail.com>
2024-04-24 03:46:03 +0200

    notes for self

--- exercises/src/test/scala/ep6.scala
+++ exercises/src/test/scala/ep6.scala
@@ -6,7 +6,9 @@ import weaver.*
 
 object ep6 extends setup.Ep6Exercises {
 
+  // TODO: Break this test by removing the parameter from the API.
   exercise(1) {
+
     //
     // Find out which operation is being called here,
     // and see what needs to be done to its parameters to make the test pass.
@@ -17,7 +19,9 @@ object ep6 extends setup.Ep6Exercises {
     assertSuccessful(GET(uri"/students/123"))
   }
 
+  // TODO: Break this test by changing the body fields, and the URL.
   exercise(2) {
+
     // Make a valid call to the operation CreateStudent.
     // Pick your favorite movie/game character as the student's name.
     // Let me know who it was in the YouTube comments ;)
@@ -38,7 +42,9 @@ object ep6 extends setup.Ep6Exercises {
     assertSuccessful(request)
   }
 
+  // TODO: break this test by turning the query param into a header.
   exercise(3) {
+
     // There's a subtle discrepancy between the request in this test, and the service definition.
     // What is it? Update the Smithy specification to match the test.
     // Do not modify this test.
@@ -49,7 +55,9 @@ object ep6 extends setup.Ep6Exercises {
     )
   }
 
+  // TODO: break this test by removing @httpPayload.
   exercise(4) {
+
     // Another discrepancy: this test should pass, but doesn't: the spec for the operation is slightly wrong.
     // Change it so that the test passes.
 
